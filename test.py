803aa8568318f099ffad1a1e4df32c24be66e28f
Andrey Voronin <andrey.voronin@cd-adapco.com>
2017-11-08 11:36:07 +0300

    update

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-str = "String"
-print str[len(str) - 1]
+from web_bot import *
+
+wb1 = web_bot("bot1")
+
+wb1.print_name()
---
+++ web_bot.py
@@ -0,0 +1,9 @@
+class web_bot:
+    name = ""
+
+    def __init__(self, name):
+        self.name = name
+
+    def print_name(self):
+        print self.name
+
